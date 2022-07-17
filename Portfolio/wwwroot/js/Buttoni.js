75a5262a3dbc4dc5c1c8e158c093c372725f1e4f
AlbinHetemi <albin.hetemi17@gmail.com>
2022-07-17 19:15:18 +0200

    Vazhdim i metutjeshem i formimit te projektit ne front end

---
+++ Portfolio/wwwroot/js/Buttoni.js
@@ -0,0 +1,17 @@
+﻿var butoni = document.getElementById("BtnUp");
+
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        butoni.style.display = "block";
+    } else {
+        butoni.style.display = "none";
+    }
+}
+
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
